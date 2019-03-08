410a7d4f30fea1acb65a24f2964cd6b184be083f
Gavin Marsh <gavinmarsh@Gavins-MacBook-Air.local>
2019-03-08 16:12:15 +0000

    added explicit file paths to __init__ functions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,13 +29,13 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import db
+    from flaskr import db
     db.init_app(app)
 
-    from . import auth
+    from flaskr import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
+    from flaskr import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
